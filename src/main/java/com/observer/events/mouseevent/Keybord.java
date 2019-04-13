59a0ef59e9bfae7de5c897a3779c05cdc0a1b9fc
曲终人散 <568868693@qq.com>
2019-04-13 17:03:36 +0800

    初始化

---
+++ src/main/java/com/observer/events/mouseevent/Keybord.java
@@ -0,0 +1,15 @@
+package com.observer.events.mouseevent;
+
+import com.observer.events.core.EventLisenter;
+
+public class Keybord extends EventLisenter {
+
+    public void down(){
+
+    }
+
+    public void up(){
+
+    }
+
+}
